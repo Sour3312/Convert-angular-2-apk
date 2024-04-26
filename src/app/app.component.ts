f48bbc77ba40b25d566791b5e3009749d2921d9f
sourabh.mahato <sourabh.mahato@mapmyindia.com>
2024-04-26 16:33:34 +0530

    simple login page added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+
+// import { LoginService } from 'src/app/_services/login/login.service';
+
+import { first } from 'rxjs';
+import Swal from 'sweetalert2';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular2apk';
+
+  constructor(
+    private router: Router,
+    // private loginService: LoginService
+  ) { }
+
+  username: string = '';
+  password: string = '';
+  errorMsg: string = '';
+  pwdFormat: string = 'password';
+  pwdVisibility: string = 'visibility_off';
+
+  loginToCRM() {
+  //  
+  alert('submitted!!')
+  }
+
+  hideShowPassword() {
+    this.pwdFormat = this.pwdFormat == 'password' ? 'text' : 'password';
+    this.pwdVisibility = this.pwdVisibility == 'visibility_off' ? 'visibility' : 'visibility_off';
+  }
+
+  callForgetPwdPage() {
+    this.router.navigate(['/forgotpassword']);
+  }
 }
